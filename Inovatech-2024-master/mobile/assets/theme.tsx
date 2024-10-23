be7eb33532dc9c452392bf9fa2db1dbd55ba5235
Fernando Melo <fernandovmelodiasl@gmail.com>
2024-10-23 00:52:21 -0300

    telas feitas

---
+++ Inovatech-2024-master/mobile/assets/theme.tsx
@@ -0,0 +1,19 @@
+import { MD3LightTheme } from 'react-native-paper';
+
+const theme = {
+  ...MD3LightTheme,
+  colors: {
+    ...MD3LightTheme.colors,
+    primary: '#4A90E2',
+    safe: '#7ED321',
+    alert: '#D9534F',
+    informative: '#F5A623',
+    text: '#d4d4d4',   
+    background: '#fafafa', 
+    
+  },
+  
+  
+};
+
+export default theme;
